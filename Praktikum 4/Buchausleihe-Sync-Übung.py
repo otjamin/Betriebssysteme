4cbe64c5b9b8f5b937c9277cacfe304f35cc6abf
Jan Nils Korte <korte.jan@hm.edu>
2024-05-10 16:52:39 +0200

    student class is no longer specialized

--- Praktikum 4/Buchausleihe-Sync-Übung.py
+++ Praktikum 4/Buchausleihe-Sync-Übung.py
@@ -11,26 +11,29 @@ class Library:
 class Student(threading.Thread):
     students_with_three_books = 0
 
-    def __init__(self, library, readingTime):
+    def __init__(self, library, readingTime, fstBooksToBorrow, sndBooksToBorrow):
         threading.Thread.__init__(self)             # Initialisiere den Thread
         self.library = library                      # Referenz auf die Bibliothek
         self.books_borrowed = 0                     # Counter für Anzahl der ausgeliehenen Bücher
         self.students_readingTime = readingTime     # Lesezeit des Studenten
+        self.fstBooksToBorrow = fstBooksToBorrow    # erste Liste der Bücher, die der Student ausleihen soll
+        self.sndBooksToBorrow = sndBooksToBorrow    # zweite Liste der Bücher, die der Student ausleihen soll
 
     def run(self):
         # Versuche, Bücher 1, 2 und 3 auszuleihen
-        if self.borrow_books([self.library.book1, self.library.book2, self.library.book3]):
+        if self.borrow_books(self.fstBooksToBorrow):
             #print(f"Student {self.name} hat Bücher 1, 2 und 3 ausgeliehen\n")
-            time.sleep(student.students_readingTime)
+            time.sleep(self.students_readingTime)
             # Gib die Bücher zurück
-            self.return_books([self.library.book1, self.library.book2, self.library.book3])
+            self.return_books(self.fstBooksToBorrow)
 
         # Versuche, Bücher 2, 3 und 4 auszuleihen
-        if self.borrow_books([self.library.book2, self.library.book3, self.library.book4]):
+        if self.borrow_books(self.sndBooksToBorrow):
             #print(f"Student {self.name} hat Bücher 2, 3 und 4 ausgeliehen\n")
-            time.sleep(student.students_readingTime)
+            time.sleep(self.students_readingTime)
             # Gib die Bücher zurück
-            self.return_books([self.library.book2, self.library.book3, self.library.book4])
+            self.return_books(self.sndBooksToBorrow)
+
 
     def borrow_books(self, books):
         for book in books:
@@ -56,10 +59,29 @@ def print_statistics(students):
 def main():
     library = Library()
 
-    numberOfStudents = int(input("Anzahl der gewünschten Studenten eingeben: "))
-    readingTime = int(input("Gewünschte Lesezeit eingeben: "))
+    allBooks = [library.book1, library.book2, library.book3, library.book4]
 
-    students = [Student(library, readingTime) for _ in range(numberOfStudents)]
+    fstBookId = (input("Geben sie die erste Ausleih-Kombination ein: "))
+
+    # Konvertiere die IDs in eine Liste von Integer-Werten
+    fstBookIds = list(map(int, fstBookId.split(',')))
+
+    # Hole die entsprechenden Buchobjekte aus der Bibliothek
+    fstBooksToBorrow = [allBooks[id - 1] for id in fstBookIds]
+
+    sndBookId = (input("Geben sie die zweite Ausleih-Kombination ein: "))
+
+    # Konvertiere die IDs in eine Liste von Integer-Werten
+    sndBookIds = list(map(int, sndBookId.split(',')))
+
+    # Hole die entsprechenden Buchobjekte aus der Bibliothek
+    sndBooksToBorrow = [allBooks[id - 1] for id in sndBookIds]
+
+    numberOfStudents = int(input("Anzahl der gewünschten Studenten (Threads) eingeben: "))
+    readingTime = float(input("Gewünschte Lesezeit eingeben: "))
+
+    students = [Student(library, readingTime, fstBooksToBorrow, sndBooksToBorrow) for _ in range(numberOfStudents)]
+    #students = [Student(library, readingTime) for _ in range(numberOfStudents)]
     for student in students:    # Starte alle Studenten-Threads
         student.start()
 
