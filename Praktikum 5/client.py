fd116bf3b87ee45e9083ecc2523c935c1af7ee66
Benjamin Otto <benjamin.otto@hm.edu>
2024-05-25 19:51:55 +0200

    send int

--- Praktikum 5/client.py
+++ Praktikum 5/client.py
@@ -1,10 +1,19 @@
 # Echo client program
 import socket
+import struct
 
 HOST = 'localhost'    # The remote host
 PORT = 50007              # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+
+    while True:
+        number_to_send = struct.pack('<h', int(input()))
+        s.sendall(number_to_send)
+
+        data = s.recv(1024)
+        print('Received {}'.format(struct.unpack('<h', data)[0]))
+
+#     s.sendall(b'Hello, world')
+#     data = s.recv(1024)
+# print('Received', repr(data))
